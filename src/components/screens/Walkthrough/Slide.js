22ae5993af05f0765da07fa622fa53e9df194f96
usmansbk <usmansbk@gmail.com>
2020-11-21 15:27:25 +0100

    chore

--- src/components/screens/Walkthrough/Slide.js
+++ src/components/screens/Walkthrough/Slide.js
@@ -1,14 +1,32 @@
 import React from 'react';
-import {View, Image} from 'react-native';
+import {View, Image, StyleSheet, Dimensions} from 'react-native';
 import {Text} from '@components/common';
+
+const {width, height} = Dimensions.get('window');
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width,
+  },
+  body: {},
+  footer: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+  },
+});
 
 export default function Slide({title, subtitle, image, footer}) {
   return (
-    <View>
-      <Image source={image} />
-      <Text>{title}</Text>
-      <Text>{subtitle}</Text>
-      <Image source={footer} />
+    <View style={styles.container}>
+      <View style={styles.body}>
+        <Image source={image} />
+        <Text variant="title">{title}</Text>
+        <Text variant="subtitle">{subtitle}</Text>
+      </View>
+      <View style={styles.footer}>
+        <Image source={footer} />
+      </View>
     </View>
   );
 }
--- src/components/screens/Walkthrough/Walkthrough.js
+++ src/components/screens/Walkthrough/Walkthrough.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import {ScrollView, View, StyleSheet, Image} from 'react-native';
+import {ScrollView, StyleSheet} from 'react-native';
 import Slide from './Slide';
 import slides from './slides';
 
 const styles = StyleSheet.create({
-  image: {
-    width: 200,
-    height: 200,
+  container: {
+    flex: 1,
   },
 });
 
 export default function Walkthrough() {
   return (
-    <ScrollView horizontal>
+    <ScrollView
+      horizontal
+      style={styles.container}
+      showsHorizontalScrollIndicator={false}>
       {slides.map((slide) => {
         return <Slide key={slide.id} {...slide} />;
       })}
