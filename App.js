9fc25298126a60565e109ab6688d32069ed36b5e
usmansbk <usmansbk@gmail.com>
2020-11-21 12:35:06 +0100

    add splashscreen

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Text, StyleSheet} from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 
 const styles = StyleSheet.create({
   demi: {
@@ -14,6 +15,13 @@ const styles = StyleSheet.create({
 });
 
 export default function App() {
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      SplashScreen.hide();
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
+
   return (
     <>
       <Text style={styles.demi}>Aking</Text>
