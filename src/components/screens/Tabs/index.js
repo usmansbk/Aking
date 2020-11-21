8ade04f5bdf7036809c5359f374127faf04984b7
usmansbk <usmansbk@gmail.com>
2020-11-21 23:15:16 +0100

    create tab bar

--- src/components/screens/Tabs/index.js
+++ src/components/screens/Tabs/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import MyTask from './MyTask';
 import Menu from './Menu';
 import Quick from './Quick';
 import Profile from './Profile';
 
+const Tab = createBottomTabNavigator();
+
 export default function Tabs() {
-  return <Menu />;
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="MyTask" component={MyTask} />
+      <Tab.Screen name="Menu" component={Menu} />
+      <Tab.Screen name="Quick" component={Quick} />
+      <Tab.Screen name="Profile" component={Profile} />
+    </Tab.Navigator>
+  );
 }
