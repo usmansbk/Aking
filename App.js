1f0db461c64e9ced5c61e155534c01221446ba8b
usmansbk <usmansbk@gmail.com>
2020-11-21 12:16:14 +0100

    add font

--- App.js
+++ App.js
@@ -1,21 +1,24 @@
 import React from 'react';
-import {Text} from 'react-native';
+import {Text, StyleSheet} from 'react-native';
+
+const styles = StyleSheet.create({
+  demi: {
+    fontFamily: 'Demi',
+  },
+  regular: {
+    fontFamily: 'Regular',
+  },
+  medium: {
+    fontFamily: 'Medium',
+  },
+});
 
 export default function App() {
   return (
     <>
-      <Text
-        style={{
-          fontFamily: 'Demi',
-        }}>
-        Aking
-      </Text>
-      <Text
-        style={{
-          fontFamily: 'Regular',
-        }}>
-        Aking
-      </Text>
+      <Text style={styles.demi}>Aking</Text>
+      <Text style={styles.medium}>Aking</Text>
+      <Text style={styles.regular}>Aking</Text>
     </>
   );
 }
