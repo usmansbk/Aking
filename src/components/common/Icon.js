4fb01ba6aeac8c6127a64cf4c6ce9d71fc957440
usmansbk <usmansbk@gmail.com>
2020-11-22 01:10:51 +0100

    custom svg color

--- src/components/common/Icon.js
+++ src/components/common/Icon.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ArrowLeft from '@assets/svgs/ArrowLeft.svg';
 
-export default function Icon({size = 24}) {
-  return <ArrowLeft width={size} height={size} />;
+export default function Icon({size = 24, color = 'black'}) {
+  return (
+    <ArrowLeft width={size} height={size} fill={color} fillSecondary="white" />
+  );
 }
