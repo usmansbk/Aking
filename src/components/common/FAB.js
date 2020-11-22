669ccad6d5641d5d81c1cd582285fcd59aa29f1f
usmansbk <usmansbk@gmail.com>
2020-11-22 13:18:46 +0100

    variant

--- src/components/common/FAB.js
+++ src/components/common/FAB.js
@@ -13,8 +13,9 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function AkingFAB({size = SIZE, onPress}) {
+export default function AkingFAB({size = SIZE, onPress, color = 'primary'}) {
   const theme = useTheme();
+  const colors = theme.palatte[color];
   return (
     <RectButton
       onPress={onPress}
@@ -24,12 +25,10 @@ export default function AkingFAB({size = SIZE, onPress}) {
           width: size,
           height: size,
           borderRadius: size / 2,
-          backgroundColor: theme.colors.red,
+          backgroundColor: colors.main,
         },
       ]}>
-      <Text style={{fontSize: size / 2}} color="contrast">
-        +
-      </Text>
+      <Text style={{fontSize: size / 2, color: colors.contrast}}>+</Text>
     </RectButton>
   );
 }
