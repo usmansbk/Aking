32a09370f0b01b305da267cfe4ffbd6933ebf111
usmansbk <usmansbk@gmail.com>
2020-11-23 11:43:20 +0100

    calendar header

--- src/components/common/Calendar/Calendar.js
+++ src/components/common/Calendar/Calendar.js
@@ -1,8 +1,17 @@
 import React from 'react';
-import {View} from 'react-native';
+import {View, StyleSheet, TouchableOpacity} from 'react-native';
 import moment from 'moment';
 import Text from '../Text';
+import Icon from '../Icon';
 import {useTheme} from '@config/theme';
+
+const styles = StyleSheet.create({
+  header: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 
 export default function Calendar() {
   return (
@@ -13,10 +22,16 @@ export default function Calendar() {
   );
 }
 
-function Header() {
+function Header({onPress, date = moment().toISOString(), expanded}) {
+  const theme = useTheme();
   return (
-    <View>
-      <Text>{moment().format('MMMM YYYY')}</Text>
-    </View>
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.header, {paddingVertical: theme.spacing.l}]}>
+      <Text variant="sectionHeader" style={{marginRight: theme.spacing.s}}>
+        {moment(date).format('MMMM YYYY').toUpperCase()}
+      </Text>
+      <Icon name={expanded ? 'chevron-down' : 'chevron-up'} size={14} />
+    </TouchableOpacity>
   );
 }
