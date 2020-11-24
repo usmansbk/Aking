feffd6717ae53cc26624dc343cdb655d6265373b
usmansbk <usmansbk@gmail.com>
2020-11-24 20:14:36 +0100

    fix opacity

--- src/components/screens/Tabs/Modal.js
+++ src/components/screens/Tabs/Modal.js
@@ -16,7 +16,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: 'rgba(0,0,0,0.5)',
+    backgroundColor: 'rgba(0,0,0,0.3)',
   },
   modalView: {
     width: '70%',
